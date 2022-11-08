988e996d535468bd679482dd021728beb58dc5d5
Yun Chen <Yunchenfly@gmail.com>
2022-11-07 21:24:53 -0500

    commit tree cb6840e9adfb99211e8b6978d3eae0ef6837819e

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2)
+}
+
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2)
+}
+
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(number) {
+    return function fareMultiplier() {
+        return number ** 2
+    }
+}
+
+const fareDoubler = function(fareMultiplier) {
+    return fareMultiplier * 2 
+}
+
+const fareTripler = function(fareMultiplier) {
+    return fareMultiplier * 3
+}
+
+function selectDifferentDrivers(arrayOfDrivers, fn) {     
+    return fn(arrayOfDrivers)
+}
